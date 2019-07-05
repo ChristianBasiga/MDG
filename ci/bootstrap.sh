fefab49261365c991e44fd611e274e231480a8e4
Prince Christian Basiga <princebasiga@gmail.com>
2019-07-04 22:32:02 -0700

    initial project setup from blank starter project

---
+++ ci/bootstrap.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e -u -x -o pipefail
+
+cd "$(dirname "$0")/../"
+
+SHARED_CI_DIR="$(pwd)/.shared-ci"
+CLONE_URL="git@github.com:spatialos/gdk-for-unity-shared-ci.git"
+PINNED_SHARED_CI_VERSION=$(cat ./ci/shared-ci.pinned)
+
+# Clone the HEAD of the shared CI repo into ".shared-ci"
+
+if [[ -d "${SHARED_CI_DIR}" ]]; then
+    rm -rf "${SHARED_CI_DIR}"
+fi
+
+mkdir "${SHARED_CI_DIR}"
+
+# Workaround for being unable to clone a specific commit with depth of 1.
+pushd "${SHARED_CI_DIR}"
+    git init
+    git remote add origin "${CLONE_URL}"
+    git fetch --depth 20 origin master
+    git checkout "${PINNED_SHARED_CI_VERSION}"
+popd
+
+# Clone the GDK for Unity repository
+
+CLONE_URI="git@github.com:spatialos/gdk-for-unity.git"
+TARGET_DIRECTORY="$(realpath $(pwd)/../gdk-for-unity)"
+PINNED_VERSION=$(cat ./gdk.pinned)
+
+rm -rf "${TARGET_DIRECTORY}"
+
+mkdir "${TARGET_DIRECTORY}"
+
+# Workaround for being unable to clone a specific commit with depth of 1.
+pushd "${TARGET_DIRECTORY}"
+    git init
+    git remote add origin "${CLONE_URI}"
+    git fetch --depth 20 origin develop
+    git checkout "${PINNED_VERSION}"
+popd
---
+++ workers/unity/Assets/Scripts/Workers/UnityGameLogicConnector.cs
@@ -0,0 +1,67 @@
+﻿using Improbable;
+using Improbable.Gdk.Core;
+using Improbable.Gdk.GameObjectCreation;
+using Improbable.Gdk.PlayerLifecycle;
+using Improbable.Gdk.TransformSynchronization;
+using Improbable.Worker.CInterop;
+using UnityEngine;
+
+namespace MDG
+{
+    public class UnityGameLogicConnector : WorkerConnector
+    {
+        public const string WorkerType = "UnityGameLogic";
+
+        private async void Start()
+        {
+            PlayerLifecycleConfig.CreatePlayerEntityTemplate = CreatePlayerEntityTemplate;
+
+            IConnectionFlow flow;
+            ConnectionParameters connectionParameters;
+
+            if (Application.isEditor)
+            {
+                flow = new ReceptionistFlow(CreateNewWorkerId(WorkerType));
+                connectionParameters = CreateConnectionParameters(WorkerType);
+            }
+            else
+            {
+                flow = new ReceptionistFlow(CreateNewWorkerId(WorkerType),
+                    new CommandLineConnectionFlowInitializer());
+                connectionParameters = CreateConnectionParameters(WorkerType,
+                    new CommandLineConnectionParameterInitializer());
+            }
+
+            var builder = new SpatialOSConnectionHandlerBuilder()
+                .SetConnectionFlow(flow)
+                .SetConnectionParameters(connectionParameters);
+
+            await Connect(builder, new ForwardingDispatcher()).ConfigureAwait(false);
+        }
+
+        protected override void HandleWorkerConnectionEstablished()
+        {
+            Worker.World.GetOrCreateSystem<MetricSendSystem>();
+            GameObjectCreationHelper.EnableStandardGameObjectCreation(Worker.World);
+
+            PlayerLifecycleHelper.AddServerSystems(Worker.World);
+        }
+
+        private static EntityTemplate CreatePlayerEntityTemplate(string workerId, byte[] serializedArguments)
+        {
+            var clientAttribute = EntityTemplate.GetWorkerAccessAttribute(workerId);
+            var serverAttribute = WorkerType;
+
+            var template = new EntityTemplate();
+            template.AddComponent(new Position.Snapshot(), clientAttribute);
+            template.AddComponent(new Metadata.Snapshot("Player"), serverAttribute);
+            TransformSynchronizationHelper.AddTransformSynchronizationComponents(template, clientAttribute);
+            PlayerLifecycleHelper.AddPlayerLifecycleComponents(template, workerId, serverAttribute);
+
+            template.SetReadAccess(UnityClientConnector.WorkerType, MobileClientWorkerConnector.WorkerType, serverAttribute);
+            template.SetComponentWriteAccess(EntityAcl.ComponentId, serverAttribute);
+
+            return template;
+        }
+    }
+}
