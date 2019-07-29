b7c2849ea442497dd82d47808c660a72a6ddcbbc
Prince Christian Basiga <princebasiga@gmail.com>
2019-07-28 22:46:09 -0700

    Imported dot4net library. Set up unity dappender and log4net config file

---
+++ workers/unity/Assets/Scripts/Logging/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEngine;
+using log4net.Config;
+
+namespace MDG.Logging
+{
+    public static class Configuration 
+    {
+      
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Configure()
+        {
+            Debug.Log("I am called");
+            FileInfo fileInfo = new FileInfo($"{Application.dataPath}/Config/log4net.xml");
+            Debug.Log(fileInfo.FullName);
+            XmlConfigurator.Configure(fileInfo);
+        }
+    }
+}
---
+++ workers/unity/Assets/Scripts/Logging/Logging.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using log4net;
+
+namespace MDG.Logging
+{
+    //Test script.
+    public class Logging : MonoBehaviour
+    {
+
+        private static readonly ILog Log = LogManager.GetLogger(typeof(MonoBehaviour));
+        // Start is called before the first frame update
+        void Start()
+        {
+               
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                Debug.Log(Log.IsDebugEnabled);
+                Log.Debug("hello");
+
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                Debug.Log(Log.IsWarnEnabled);
+                Log.Warn("world");
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log(Log.IsErrorEnabled);
+                Log.Error("error");
+            }
+        }
+    }
+}
