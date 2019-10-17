2de3795255f7147e3494b0954b1465c5c5c687c6
Prince Christian Basiga <princebasiga@gmail.com>
2019-10-16 20:37:40 -0700

    implemented quad tree for spatial partitioning and position system

---
+++ workers/unity/Assets/Scripts/Common/DataStructures/QuadTree.cs
@@ -0,0 +1,178 @@
+﻿using Improbable;
+using Improbable.Gdk.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MDG.Common.Datastructures
+{
+
+    // This will be a quad tree with 
+    public class QuadTree
+    {
+        // Also need dimensions.
+        // Maybe flow is I get QuadNodes, then using EntityId I get collider component
+        // to get dimensions of that entity in that region.
+        // I don't care for dimensions of each point within quad tree.
+        // hell potentially from that quad tree I could create another to get the actual colliding entities.
+        // Look into GJK as that will likely be better
+        // either way broad phase, could be firs this, then dot product to see if potentially overlapping.
+        // that won't result in false negative but false positives which I'll handle in narrow phase.
+        public struct QuadNode
+        {
+            public EntityId entityId;
+            public Vector3f position;
+        }
+        // Information about this region.
+        int capacity;
+        Vector3f dimensions;
+        Vector3f center;
+        // Four children representing regions wihin this region.
+        QuadTree northWest;
+        QuadTree northEast;
+        QuadTree southWest;
+        QuadTree southEast;
+
+        Dictionary<EntityId, Vector3f> entitiesInThisRegion;
+        #region Interfacing code
+        public QuadTree(int capacity, Vector3f dimensions, Vector3f center)
+        {
+            this.capacity = capacity;
+            this.dimensions = dimensions;
+            this.center = center;
+            entitiesInThisRegion = new Dictionary<EntityId, Vector3f>();
+        }
+
+        public List<QuadNode> FindEntities(Vector3f queryPosition)
+        {
+            List<QuadNode> entities = new List<QuadNode>();
+
+            if (!IsWithinRegion(queryPosition))
+            {
+                return entities;
+            }
+            // Check if subdivided.
+            if (northEast == null)
+            {
+                //Recursively find them. If not exist in region, returns empty so no change.
+                entities.AddRange(northEast.FindEntities(queryPosition));
+                entities.AddRange(northWest.FindEntities(queryPosition));
+                entities.AddRange(southEast.FindEntities(queryPosition));
+                entities.AddRange(southWest.FindEntities(queryPosition));
+            }
+            else
+            {
+                foreach(KeyValuePair<EntityId, Vector3f> keyValuePair in entitiesInThisRegion)
+                {
+                    QuadNode quadNode = new QuadNode
+                    {
+                        entityId = keyValuePair.Key,
+                        position = keyValuePair.Value
+                    };
+                }
+            }
+            return entities;
+        }
+
+        // Instead of moving via initial and new, can just pass transformation?
+        // Actually no matter what may need to go back up tree, so traversing downward twice is fine just incase.
+        // Unless I have it be own
+        public void MoveEntity(EntityId entityId, Vector3f originalPosition, Vector3f newPosition)
+        {
+            Remove(entityId, originalPosition);
+            Insert(entityId, newPosition);
+        }
+
+        public bool Insert(EntityId entityId, Vector3f position)
+        {
+            if (!IsWithinRegion(position))
+            {
+                return false;
+            }
+            // If one region is undefined, then I haven't subdivided.
+            if (northEast == null)
+            {
+                if (entitiesInThisRegion.ContainsKey(entityId))
+                {
+                    throw new System.Exception($" The entity with id {entityId} is already in this quadtree. " +
+                        $"Please use MoveEntity insted");
+                }
+                if (entitiesInThisRegion.Count + 1 > capacity)
+                {
+                    SubDivide();
+                }
+            }
+            // This is if we already subdivided before, or subdivided within this stack frame.
+            if (northEast != null)
+            {
+                // Recursively call insert in children, until one of the calls results in true
+                // short circuiting the rest.
+                return northWest.Insert(entityId, position)
+                    || northEast.Insert(entityId, position)
+                    || southWest.Insert(entityId, position)
+                    || southEast.Insert(entityId, position);
+            }
+            entitiesInThisRegion.Add(entityId, position);
+            return false;
+        }
+
+        // Maybe should create private try remove.
+        public bool Remove(EntityId entityId, Vector3f position)
+        {
+            if (!IsWithinRegion(position))
+            {
+                return false;
+            }
+            if (northEast == null)
+            {
+                return northWest.Remove(entityId, position)
+                    || northEast.Remove(entityId, position)
+                    || southEast.Remove(entityId, position)
+                    || southWest.Remove(entityId, position);
+            }
+            else
+            {
+                entitiesInThisRegion.Remove(entityId);
+                return true;
+            }
+        }
+
+
+        #endregion
+
+
+        #region Helper private functions
+
+        // Just keeping it in interface functions fine, them returning bools NOT huge deal.
+        private bool TryRemove(EntityId entityId, Vector3f position)
+        {
+            return false;
+        }
+
+        // Insantiates 4 children to create inner regions.
+        private void SubDivide()
+        {
+            float width = dimensions.X;
+            float height = dimensions.Z;
+            Vector3f newDimensions = new Vector3f(width / 2, 0, height / 2);
+            // Dimensions also needs to be halved.
+            northEast = new QuadTree(capacity, newDimensions, new Vector3f(center.X + width / 4, 0, center.Z + height / 4 ));
+            northWest = new QuadTree(capacity, newDimensions, new Vector3f(center.X - width / 4, 0, center.Z + height / 4));
+            southEast = new QuadTree(capacity, newDimensions, new Vector3f(center.X + width / 4, 0,  center.Z - height / 4));
+            southWest = new QuadTree(capacity, newDimensions, new Vector3f(center.X - width / 4, 0, center.Z - height / 4));
+        }
+
+        private bool IsWithinRegion(Vector3f position)
+        {
+            float width = dimensions.X;
+            float height = dimensions.Z;
+
+            return (position.X <= center.X + width / 2)
+                && (position.X >= center.X - width / 2)
+                && (position.Z <= center.Z + height / 2)
+                && (position.Z >= center.Z - height / 2);
+        }
+        #endregion
+    }
+}
---
+++ workers/unity/Assets/Scripts/Common/Systems/PositionManagement/PositionSystem.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Jobs;
+using MDG.Common.Datastructures;
+using Improbable;
+using Improbable.Gdk.Core;
+using MdgSchema.Common;
+using PositionSchema = MdgSchema.Common.Position;
+namespace MDG.Common.Systems
+{
+    /// <summary>
+    /// System that runs on server, manages the spatial partitioned collection of all entities with entity
+    /// transform component. It also does ALL updates to position. All position updates must request this system.
+    /// Latter might not be good idea. All just change velocity, but it only ever actually applies when hits this system.
+    /// </summary>
+    public class PositionSystem : ComponentSystem
+    {
+        struct UpdatePayload
+        {
+            public EntityId entityUpdating;
+            public Vector3f newPosition;
+        }
+
+        EntityQuery toAddToTreeQuery;
+        EntityQuery applyVelocityQuery;
+        // This will be dimensions of world down line, injected here.
+        readonly Vector3f initialDimensions = new Vector3f(50, 0, 50);
+        readonly int regionCapacity = 10;
+        QuadTree quadTree;
+        NativeQueue<UpdatePayload> updateQueue;
+
+        float shakeBuffer = 1000;
+        float timeSinceLastShake = 0;
+
+        
+        public List<QuadNode> querySpatialPartition(Vector3f position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Returns collective region  that the entityId belongs to.
+        public List<QuadNode> querySpatialPartition(EntityId entityId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            quadTree = new QuadTree(regionCapacity, initialDimensions, new Vector3f(initialDimensions.X / 2, 0, initialDimensions.Z / 2));
+            updateQueue = new NativeQueue<UpdatePayload>();
+            toAddToTreeQuery = GetEntityQuery(
+                ComponentType.ReadOnly<NewlyAddedSpatialOSEntity>(),
+                ComponentType.ReadOnly<EntityTransform.Component>(),
+                ComponentType.ReadOnly<SpatialEntityId>()
+                );
+            applyVelocityQuery = GetEntityQuery(
+                ComponentType.ReadOnly<PositionSchema.LinearVelocity.Component>(),
+                ComponentType.ReadOnly<PositionSchema.AngularVelocity.Component>(),
+                ComponentType.ReadWrite<EntityTransform.Component>(),
+                ComponentType.ReadOnly<EntityTransform.ComponentAuthority>()
+                );
+            applyVelocityQuery.SetFilter(EntityTransform.ComponentAuthority.Authoritative);
+        }
+
+
+        struct ApplyVelocityJob : IJobForEach<PositionSchema.LinearVelocity.Component, PositionSchema.AngularVelocity.Component,
+            EntityTransform.Component>
+        {
+            public float deltaTime;
+            public void Execute(ref PositionSchema.LinearVelocity.Component linearVelocityComponent, 
+                ref PositionSchema.AngularVelocity.Component angularVelocityComponent,
+                ref EntityTransform.Component entityTransform)
+            {
+                entityTransform.Position += linearVelocityComponent.Velocity * deltaTime;
+                entityTransform.Rotation += angularVelocityComponent.AngularVelocity * deltaTime;
+            }
+        }
+
+
+        protected override void OnUpdate()
+        {
+            // This should handle all requests for updates to position.
+            // as well as running job to update position via velocity.
+            float deltaTime = Time.deltaTime;
+            ApplyVelocityJob applyVelocityJob = new ApplyVelocityJob
+            {
+                deltaTime = deltaTime
+            };
+            applyVelocityJob.Run(applyVelocityQuery);
+
+            UpdateEntitiesInTree();
+            AddNewEntitiesToQuadTree();
+
+            timeSinceLastShake += deltaTime;
+            if (timeSinceLastShake >= shakeBuffer)
+            {
+                ShakeQuadTree();
+            }
+        }
+
+        // Iterates through newly added entities and inserts into quad tree.
+        // Main issue here, is position not updated yet. So will be adding the entity, then immediemtly moving it next frame.
+        // we'll see how that goes. May need to delay it later.
+        private void AddNewEntitiesToQuadTree()
+        {
+            Entities.With(toAddToTreeQuery).ForEach((ref SpatialEntityId spatialEntityId, ref EntityTransform.Component entityTransform) =>
+            {
+                quadTree.Insert(spatialEntityId.EntityId, entityTransform.Position);
+            });
+        }
+
+        // Runs through update queues and updates them in batches in quad tree.
+        // over a couple frames is fine.
+        private void UpdateEntitiesInTree()
+        {
+            // For each entityId to update, first check using the position to update to if still within region.
+            while (updateQueue.Count > 0)
+            {
+                UpdatePayload updatePayload = updateQueue.Dequeue();
+
+                List<QuadNode> entitiesInRegion = quadTree.FindEntities(updatePayload.newPosition);
+                QuadNode lastRecord = entitiesInRegion.Find((QuadNode quadNode) =>
+                {
+                    return quadNode.entityId.Equals(updatePayload.entityUpdating);
+                });
+
+                // Then no need for update, still in same region.
+                if (lastRecord != null)
+                {
+                    continue;
+                }
+                // Otherwise get entity from old region.
+                lastRecord = quadTree.FindEntity(updatePayload.entityUpdating);
+
+                //And remove from last and insert in new.
+                quadTree.MoveEntity(updatePayload.entityUpdating, lastRecord.position, updatePayload.newPosition);
+            }
+        }
+
+        // Prunes tree of any entities in quad tree that are no longer active.
+        // thus reducing size. Maybe down road will clean to close any subdivisions.
+        // or keep subdivided, but still prune.
+        private void ShakeQuadTree()
+        {
+            timeSinceLastShake = 0;
+        }
+    }
+}
