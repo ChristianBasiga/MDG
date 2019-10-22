2e852c71e07faf9a59f69e37b2565b16f082faf5
Prince Christian Basiga <princebasiga@gmail.com>
2019-10-22 16:10:02 -0700

    Created InvaderHud monobehaviour and added events to ClientGameObject creator for updating ui easier

--- workers/unity/Assets/Scripts/Factories/ClientGameObjectCreator.cs
+++ workers/unity/Assets/Scripts/Factories/ClientGameObjectCreator.cs
@@ -33,6 +33,11 @@ namespace MDG
     // Use zenject to install stuff here.
     public class ClientGameObjectCreator : IEntityGameObjectCreator
     {
+        public delegate void EntityChangeEventHandler(EntityId entityId);
+
+        // For others to know when thish happens.
+        public event EntityChangeEventHandler OnEntityAdded;
+        public event EntityChangeEventHandler OnEntityDeleted;
         // Storing here prob fine actually.
         public Dictionary<EntityId, List<GameObject>> EntityToGameObjects { private set; get; }
         // Get from pool down line.
@@ -179,12 +184,19 @@ namespace MDG
                 _default.OnEntityCreated(entity, linker);
                 return;
             }
+
+            OnEntityAdded?.Invoke(entity.SpatialOSEntityId);
         }
 
+        // Also needs to know about this when Units are deleted. Easieset is to just run qury.
+        // This might end up doing too much, but this could have ref to respective HUD
+        // then once one of these happens, checks if what was removed / added was unit and update accordingly.
+        // Feels like spaghetti tho.
         public void OnEntityRemoved(EntityId entityId)
         {
             _default.OnEntityRemoved(entityId);
             List<GameObject> linkedGameObjects;
+
             if (EntityToGameObjects.TryGetValue(entityId, out linkedGameObjects))
             {
                 // Destroy GameObject represnting it and remove from mappings.
@@ -194,6 +206,7 @@ namespace MDG
                 }
             }
             EntityToGameObjects.Remove(entityId);
+            OnEntityDeleted?.Invoke(entityId);
         }
 
         GameObject CreateEntityObject(SpatialOSEntity entity, EntityGameObjectLinker linker, string pathToEntity, Transform parent = null, System.Type[] ecsComponents = null)
---
+++ workers/unity/Assets/Scripts/Invader/Monobehaviours/UserInterface/InvaderHud.cs
@@ -0,0 +1,46 @@
+﻿using Improbable.Gdk.Core;
+using Improbable.Gdk.Subscriptions;
+using MDG;
+using MdgSchema.Common.Point;
+using MdgSchema.Units;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MDG.Invader.Monobehaviours
+{
+    public class InvaderHud : MonoBehaviour
+    {
+        [Require] PointReader pointReader;
+        Text numberOfUnits;
+        Text pointText;
+        EntityQuery unitQuery;
+        void Start()
+        {
+            pointReader.OnValueUpdate += UpdatePointText;
+            // Setup for updating unit count.
+            LinkedEntityComponent linkedEntityComponent = GetComponent<LinkedEntityComponent>();
+            unitQuery = linkedEntityComponent.Worker.EntityManager.CreateEntityQuery(
+                ComponentType.ReadOnly<Unit.Component>()
+                );
+            UnityClientConnector unityClientConnector = GameObject.Find("ClientWorker").GetComponent<UnityClientConnector>();
+            unityClientConnector.clientGameObjectCreator.OnEntityAdded += UpdateUnitCount;
+            unityClientConnector.clientGameObjectCreator.OnEntityDeleted += UpdateUnitCount;
+        }
+
+        private void UpdatePointText(int pointValue)
+        {
+            pointText.text = pointValue.ToString();
+        }
+
+        // Just calculate entity count each time, should update without recreating query.
+        // Could only calculate count if the updated is of Unit type. But that's small change can make later.
+        private void UpdateUnitCount(EntityId entityId)
+        {
+            int newUnitCount = unitQuery.CalculateEntityCount();
+            numberOfUnits.text = newUnitCount.ToString();
+        }
+    }
+}
